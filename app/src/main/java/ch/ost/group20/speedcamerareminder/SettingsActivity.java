98d02507200fef8c341888374194e987dfef4eb0
Christian Rutzer <chrigi.rutzer@hotmail.com>
2020-10-20 15:11:06 +0200

    update settings activity

--- app/src/main/java/ch/ost/group20/speedcamerareminder/SettingsActivity.java
+++ app/src/main/java/ch/ost/group20/speedcamerareminder/SettingsActivity.java
@@ -5,7 +5,11 @@ import android.view.MenuItem;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import ch.ost.group20.speedcamerareminder.timepicker.TimePreference;
+import ch.ost.group20.speedcamerareminder.timepicker.TimePreferenceDialogFragmentCompat;
 
 public class SettingsActivity extends AppCompatActivity {
     public  static final String KEY_NOTIFICATION_SWITCH = "notification_switch_key";
@@ -30,6 +34,28 @@ public class SettingsActivity extends AppCompatActivity {
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
+        }
+
+        @Override
+        public void onDisplayPreferenceDialog(Preference preference) {
+            DialogFragment dialogFragment = null;
+            if (preference instanceof TimePreference)
+            {
+                dialogFragment = new TimePreferenceDialogFragmentCompat();
+                Bundle bundle = new Bundle(1);
+                bundle.putString("key", preference.getKey());
+                dialogFragment.setArguments(bundle);
+            }
+
+            if (dialogFragment != null)
+            {
+                dialogFragment.setTargetFragment(this, 0);
+                dialogFragment.show(this.getFragmentManager(), "android.support.v7.preference.PreferenceFragment.DIALOG");
+            }
+            else
+            {
+                super.onDisplayPreferenceDialog(preference);
+            }
         }
     }
 
---
+++ app/src/main/java/ch/ost/group20/speedcamerareminder/timepicker/TimePreferenceDialogFragmentCompat.java
@@ -0,0 +1,51 @@
+package ch.ost.group20.speedcamerareminder.timepicker;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.TimePicker;
+
+import androidx.preference.DialogPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceDialogFragmentCompat;
+
+public class TimePreferenceDialogFragmentCompat extends PreferenceDialogFragmentCompat implements DialogPreference.TargetFragment
+{
+    TimePicker timePicker = null;
+
+    @Override
+    protected View onCreateDialogView(Context context)
+    {
+        timePicker = new TimePicker(context);
+        return (timePicker);
+    }
+
+    @Override
+    protected void onBindDialogView(View v)
+    {
+        super.onBindDialogView(v);
+        timePicker.setIs24HourView(true);
+        TimePreference pref = (TimePreference) getPreference();
+        timePicker.setHour(pref.hour);
+        timePicker.setMinute(pref.minute);
+    }
+
+    @Override
+    public void onDialogClosed(boolean positiveResult)
+    {
+        if (positiveResult)
+        {
+            TimePreference pref = (TimePreference) getPreference();
+            pref.hour = timePicker.getHour();
+            pref.minute = timePicker.getMinute();
+
+            String value = TimePreference.timeToString(pref.hour, pref.minute);
+            if (pref.callChangeListener(value)) pref.persistStringValue(value);
+        }
+    }
+
+    @Override
+    public Preference findPreference(CharSequence charSequence)
+    {
+        return getPreference();
+    }
+}
